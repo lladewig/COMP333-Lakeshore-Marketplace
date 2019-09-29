7c07f7d16b19e0f2dd17ea65bfccd697782700fd
MahaBKT <malzanbaqi@luc.edu>
2019-09-29 16:15:10 -0500

    product & partner domain

--- src/com/marketplace/domain/Partner.java
+++ src/com/marketplace/domain/Partner.java
@@ -2,4 +2,30 @@ package com.marketplace.domain;
 
 public class Partner {
 
+	private int partner_ID;
+	private static String partner_name;
+	private static String partner_type;
+	private static String partner_details;
+	
+	public PartnerDAL(String partner_name, String partner_type, String partner_details) {
+		this.partner_name = partner_name;
+		this.partner_type= partner_type;
+		this.partner_details = partner_detaols;
+		
+	}
+	public Partner() {
+		
+	}
+	public int getPartner_ID() {
+		return partner_ID;
+	}
+	public String getPartner_name() {
+		return partner_name;
+	}
+	public String getPartner_type() {
+		return partner_type;
+	}
+	public String getPartner_details;
+		return partner_details;
+}
 }
--- src/com/marketplace/domain/Product.java
+++ src/com/marketplace/domain/Product.java
@@ -1,5 +1,30 @@
 package com.marketplace.domain;
 
 public class Product {
-
+	private int product_ID;
+	private static String product_name;
+	private static String product_detail;
+	private static String product_cost;
+	
+	public Product(String product_name, String product_detail, String product_cost ) {
+		this.product_cost= product_cost;
+		product_detail = product_detail;
+		product_name = product_name;
+				
+	}
+	public Product() {
+		
+	}
+	public int getProduct_ID() {
+		return product_ID;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public String getProduct_detail() {
+		return product_detail;
+	}
+	public String getProduct_cost() {
+		return product_cost;
+	}
 }
