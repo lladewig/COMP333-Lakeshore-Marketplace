b626caec98eff669a456034659f17c016578de23
Lindsay Ladewig <lladewig@luc.edu>
2019-09-29 01:42:45 -0500

    added partner/product. updated bugged sql tables, db model

---
+++ src/com/marketplace/domain/PartnerLogic.java
@@ -0,0 +1,32 @@
+package com.marketplace.domain;
+
+import java.util.List;
+
+import com.marketplace.dal.PartnerDAL;
+
+public class PartnerLogic {
+	
+	public Partner getPartnerByID(int partnerID) {
+		return PartnerDAL.getPartnerByID(partnerID);
+	}
+	
+	public List<Partner> getAllPartners(){
+		return PartnerDAL.getAllPartners();
+	}
+
+	public Partner addPartner(String partnerName, String partnerType, String partnerDetails) {
+		return PartnerDAL.addPartner(partnerName, partnerType, partnerDetails);
+	}
+	
+	public Partner deletePartner(int partnerID) {
+		return PartnerDAL.deletePartner(partnerID);
+	}
+	
+	public Partner updatePartnerName(int partnerID, String partnerName) {
+		return PartnerDAL.updatePartnerName(partnerID, partnerName);
+	}
+	
+	public Partner updatePartnerDetails(int partnerID, String partnerDetails) {
+		return PartnerDAL.updatePartnerDetails(partnerID, partnerDetails);
+	}
+}
--- src/com/marketplace/domain/Product.java
+++ src/com/marketplace/domain/Product.java
@@ -1,5 +1,60 @@
 package com.marketplace.domain;
 
 public class Product {
+	private int productID;
+	private Partner partner;
+	private String productName;
+	private String productDescription;
+	private double productCost;
 
+	public Product(Partner partner, String productName, String productDescription, double productCost) {
+		this.partner = partner;
+		this.productName = productName;
+		this.productDescription = productDescription;
+		this.productCost = productCost;
+	}
+	
+	public Product() {
+		
+	}
+	
+	public int getproductID() {
+		return productID;
+	}
+	
+	public void setproductID(int productID) {
+		this.productID = productID;
+	}
+	
+	public String getproductName() {
+		return productName;
+	}
+	
+	public void setproductName(String productName) {
+		this.productName = productName;
+	}
+	
+	public Partner getpartner() {
+		return partner;
+	}
+	
+	public void setpartner(Partner partner) {
+		this.partner = partner;
+	}
+	
+	public String getproductDescription() {
+		return productDescription;
+	}
+	
+	public void setproductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	
+	public double getproductCost() {
+		return productCost;
+	}
+	
+	public void setproductCost(double productCost) {
+		this.productCost = productCost;
+	}
 }
