989c891cfb62cf708683b883b3249a799a131bee
Lindsay Ladewig <lladewig@luc.edu>
2019-12-06 15:57:04 -0600

    added links to customer profile

--- Client/customer_profile.js
+++ Client/customer_profile.js
@@ -1,13 +1,17 @@
+var initialResponse;
+var customerID;
+
 $(document).ready(function(){
     $('.header').height($(window).height());
 
     var decodedCookie = decodeURIComponent(document.cookie);
     var cookie = decodedCookie.split(';');
     var customerIDString = cookie[0].split("=");
-    var customerID = customerIDString[1];
+    customerID = customerIDString[1];
     var url = "http://localhost:8081/customerservice/customers/" + customerID;
     axios.get(url)
     .then(response =>{
+        initialResponse = response;
         document.getElementById("thanks").innerHTML = "Thanks for signing up, " + response.data.Customer.firstName;
         document.getElementById("firstName").innerHTML = "First name: " + response.data.Customer.firstName;
         document.getElementById("lastName").innerHTML = "Last name: " + response.data.Customer.lastName;
@@ -17,3 +21,57 @@ $(document).ready(function(){
         console.log(error);
     }); 
 });
+
+function performCustomerFirstNameUpdate(){
+    const firstNameURL = initialResponse.data.Customer.link[1].url;
+    axios.put(firstNameURL, {
+        'CustomerRequest': {
+            'customerID': customerID,
+            'firstName': document.getElementById("first-name").value
+        }
+    })
+    .then(response =>{
+        console.log(response.status);
+        document.getElementById("firstName").innerHTML = "First name: " + response.data.Customer.firstName;
+    })
+}
+
+function performCustomerLastNameUpdate(){
+    const lastNameURL = initialResponse.data.Customer.link[2].url;
+    console.log(lastNameURL);
+    axios.put(lastNameURL, {
+        'CustomerRequest': {
+            'customerID': customerID,
+            'lastName': document.getElementById("last-name").value
+        }
+    })
+    .then(response =>{
+        console.log(response.status);
+        document.getElementById("lastName").innerHTML = "Last name: " + response.data.Customer.lastName;
+    })
+}
+
+function performCustomerPhoneNumberUpdate(){
+    const phoneNumberURL = initialResponse.data.Customer.link[3].url;
+    console.log(phoneNumberURL);
+    axios.put(phoneNumberURL, {
+        'CustomerRequest': {
+            'customerID': customerID,
+            'phoneNumber': document.getElementById("phone-number").value
+        }
+    })
+    .then(response =>{
+        console.log(response.status);
+        document.getElementById("phoneNumber").innerHTML = "Phone number: " + response.data.Customer.phoneNumber;
+    })
+}
+
+function performCustomerDelete(){
+    const deleteURL = initialResponse.data.Customer.link[0].url;
+    axios.delete(deleteURL)
+    .then(response =>{
+        console.log("aaaah im deleted");
+        console.log(response.status);
+        window.location.href = "http://localhost:8082/index.html";
+    })
+}
