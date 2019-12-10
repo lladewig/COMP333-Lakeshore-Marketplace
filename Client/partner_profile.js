178dfb0bd9f07dafb47a448c19ae79cd0403f3df
Lindsay Ladewig <lladewig@luc.edu>
2019-12-09 21:04:03 -0600

    fixed bugs, added partner product add

--- Client/partner_profile.js
+++ Client/partner_profile.js
@@ -8,12 +8,13 @@ $(document).ready(function(){
     var cookie = decodedCookie.split(';');
     var partnerIDString = cookie[0].split("=");
     partnerID = partnerIDString[1];
-    var url = "http://localhost:8081/customerservice/partners/" + partnerID;
+    var url = "http://localhost:8081/partnerservice/partners/" + partnerID;
     axios.get(url)
     .then(response =>{
         initialResponse = response;
+        console.log(response);
         document.getElementById("thanks").innerHTML = "Thanks for signing up, " + response.data.Partner.partnerName;
-        document.getElementById("parterName").innerHTML = "Partner name: " + response.data.Partner.partnerName;
+        document.getElementById("partnerName").innerHTML = "Partner name: " + response.data.Partner.partnerName;
         document.getElementById("partnerType").innerHTML = "Partner type: " + response.data.Partner.partnerType;
         document.getElementById("details").innerHTML = "Details: " + response.data.Partner.partnerDetails;
     })
@@ -23,7 +24,7 @@ $(document).ready(function(){
 });
 
 function performPartnerNameUpdate(){
-    const partnerNameURL = initialResponse.data.Partner.link[1].url;
+    const partnerNameURL = initialResponse.data.Partner.link[0].url;
     console.log(partnerNameURL);
     axios.put(partnerNameURL, {
         'PartnerRequest': {
@@ -34,44 +35,31 @@ function performPartnerNameUpdate(){
     .then(response =>{
         console.log(response.status);
         document.getElementById("partnerName").innerHTML = "Partner name: " + response.data.Partner.partnerName;
-        $('#partner-name').val('')
+        $('#partner-name').val('');
     })
 }
 
-function performPartnerTypeUpdate(){
-    const partnerTypeURL = initialResponse.data.Partner.link[2].url;
-    console.log(partnerTypeURL);
-    axios.put(partnerTypeURL, {
-        'PartnerRequest': {
-            'partnerID': partnerID,
-            'partnerType': document.getElementById("partner-type").value
-        }
-    })
-    .then(response =>{
-        console.log(response.status);
-        document.getElementById("partnerType").innerHTML = "Partner Type: " + response.data.Partner.partnerType;
-        $('#partner-type').val('')
-    })
-}
 
 function performPartnerDetailsUpdate(){
-    const detailsURL = initialResponse.data.Partner.link[3].url;
+    const detailsURL = initialResponse.data.Partner.link[1].url;
     console.log(detailsURL);
     axios.put(detailsURL, {
         'PartnerRequest': {
             'partnerID': partnerID,
-            'details': document.getElementById("details").value
+            'partnerDetails': document.getElementById("partner-details").value
         }
     })
     .then(response =>{
         console.log(response.status);
+        console.log(response);
+        console.log(document.getElementById("partner-details").value);
         document.getElementById("details").innerHTML = "Details: " + response.data.Partner.partnerDetails;
-        $('#details').val('')
+        $('#partner-details').val('');
     })
 }
 
 function performPartnerDelete(){
-    const deleteURL = initialResponse.data.Partner.link[0].url;
+    const deleteURL = initialResponse.data.Partner.link[2].url;
     axios.delete(deleteURL)
     .then(response =>{
         console.log("aaaah im deleted");
